0d2e78ca8e9e8b1d55ae8f470f1c032854531c40
Rozena Akter <153695774+rozenaakter@users.noreply.github.com>
2024-10-29 21:36:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let timer ;
+let minutes = 25;
+let seconds = 0
+
+function startTimer() {
+    timer = setInterval(updateTimer, 1000);
+}
+
+function pauseTimer() {
+    clearInterval(timer);
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    minutes = 25;
+    seconds = 0;
+    updateDisplay()
+}
+
+function updateTimer(){
+    if (minutes === 0 && seconds === 0) {
+        clearInterval(timer);
+        alert('Promodoro completed ! Take a break.');
+    } else if (seconds === 0) {
+        minutes--;
+        seconds = 59;
+    } else {
+        seconds--;
+    }
+
+    updateDisplay()
+}
+
+function updateDisplay() {
+    const formattedMinutes = String(minutes).padStart(2,'0');
+    const formatedSeconds = String(seconds).padStart(2,'0');
+
+    document.getElementById('timer').textContent = `${formattedMinutes}:${formatedSeconds}`;
+};
